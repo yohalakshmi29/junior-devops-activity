0bec6560727ad1c7cd34b9434408dcdad614f828
yohalakshmi <yohalakshmi29@gmail.com>
2026-02-15 00:51:34 +0530

    Add Docker build and optional push to CI/CD workflow

--- node-service/index.js
+++ node-service/index.js
@@ -3,5 +3,5 @@ const axios = require("axios");
 const pythonUrl = process.env.PYTHON_URL || "http://python-service:5000";
 
 axios.get(`${pythonUrl}/`)
-  .then(res => console.log(res.data))
-  .catch(err => console.error(err));
+  .then((res) => console.log(res.data))
+  .catch((err) => console.error(err));
